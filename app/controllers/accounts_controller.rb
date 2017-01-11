63d45b8d57655eaa23f24120f0738446f52607ec
Ian Fox <ianfox41@gmail.com>
2017-01-11 13:51:25 +0200

    fixup! Create Account controller for CRUD and auth

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,15 @@
-class AccountController < ApplicationController
+class AccountsController < ApplicationController
 
   def update_donation_amount
+    render json: {}
   end
 
   def amount_donated
+    render json: {}
   end
 
   def get_token
+    render json: {}
   end
 
   def clear_token
