8729fe28f06147053c9ac94a6bbf3d7b57dfc36d
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2017-01-17 21:22:59 +0200

    test sockets

--- app/controllers/map_search_controller.rb
+++ app/controllers/map_search_controller.rb
@@ -1,6 +1,10 @@
 class MapSearchController < WebsocketRails::BaseController
   def new_tester
     puts("hello world")
+    puts(message)
     print("helo world 2")
+    puts(message)
+    Coin.within(0.185, :origin => [32.064510, 34.774286])
   end
+
 end
